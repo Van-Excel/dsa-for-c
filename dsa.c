ea63d282e85faf1a548b4d597e337a9acf166dbe
Van-Excel <vanexcel.investments@gmail.com>
2025-06-14 00:13:08 +0000

    recursion to add data

--- dsa.c
+++ dsa.c
@@ -18,7 +18,7 @@ void create_linked_list(int A[], int n)
 {
 
     // we need some variables
-    int i;                   // for traversing the array
+    int i;                    // for traversing the array
     struct Node *temp, *last; // temporary pointer variables for traversing linked list and assigning address of last node
 
     // head = (struct Node*) malloc(sizeof(struct Node)); // this will allocated 12 bytes of memory in the heap and return the address
@@ -35,32 +35,38 @@ void create_linked_list(int A[], int n)
         temp = malloc(sizeof(struct Node)); // create new node
         temp->data = A[i];                  // assign the next value in the array to the newly created node
         temp->next = NULL;                  // assign NULL to the pointer in the newly created node
-        last->next = temp;                   // link previous node to newly created node
+        last->next = temp;                  // link previous node to newly created node
         last = temp;                        // we move the pointer of last variable to point to the newly created node
     }
 }
 
-//display function - traverse the linked list
+// display function - traverse the linked list
 
-void display_linked_list(struct Node* p){
-    while (p != NULL){
+void display_linked_list(struct Node *p)
+{
+    while (p != NULL)
+    {
         printf("%d->", p->data);
-       
+
         p = p->next;
     }
     printf("\n");
 }
 
- // recursive function to traverse linked list
- void display_recursive(struct Node* p){
-    if (p != NULL){
+// recursive function to traverse linked list
+void display_recursive(struct Node *p)
+{
+    if (p != NULL)
+    {
         printf("%d->", p->data);
         display_recursive(p->next);
-    }else{
-        //printf("\n");
+    }
+    else
+    {
+        // printf("\n");
         printf(" NULL");
     }
- }
+}
 
 //  display_forward(3)
 // │
@@ -72,7 +78,6 @@ void display_linked_list(struct Node* p){
 // │       ├── prints: 7->
 // │       ├── display_forward(NULL)
 // │           └── prints: NULL
-
 
 // Call Stack Builds:
 // display_forward(3)
@@ -87,24 +92,24 @@ void display_linked_list(struct Node* p){
 // │ display_forward(3)     │ ← bottom
 // └────────────────────────┘
 
-
-
-
 // function to traverse linked list and print node data in reverse
- void display_recursive_reverse(struct Node* p){
-    if (p != NULL){
+void display_recursive_reverse(struct Node *p)
+{
+    if (p != NULL)
+    {
         display_recursive(p->next); // will print the linked list in reverse
         printf("%d->", p->data);
-       
-    }else{
-        //printf("\n");
+    }
+    else
+    {
+        // printf("\n");
         printf(" NULL");
         printf("\n");
     }
     printf("\n");
- }
+}
 // how the recursion works when we switch the function call statement
-//  display_recursive(3) → 
+//  display_recursive(3) →
 //     display_recursive(5) →
 //         display_recursive(7) →
 //             display_recursive(NULL) → prints "NULL"
@@ -119,18 +124,17 @@ void display_linked_list(struct Node* p){
 
 // Putting printf() before the recursive call prints in forward order
 
-
 // function to count the nodes in a linked list
 
- int count_nodes(struct Node* p){
+int count_nodes(struct Node *p)
+{
     int count;
     count = 0;
     printf("\n");
-    while(p != NULL){
+    while (p != NULL)
+    {
         count++;
         p = p->next;
-
-
     }
     printf("The number of nodes are: %d \n", count);
 
@@ -138,8 +142,69 @@ void display_linked_list(struct Node* p){
 
     // time complexity - O(n)
     // space complexity - O(1)
- }
+}
 
+int recursive_count(struct Node *p)
+{
+    if (p == NULL)
+        return 0;
+    return recursive_count(p->next) + 1;
+}
+
+//  int recursive_count(struct Node* p){
+//     if (p == NULL){
+//         return 0;
+
+//     }else{
+//         return recursive_count(p->next) + 1;
+//     }
+
+//     // time complexity = O(n)
+//     // space complexity = O(n)
+//     //more costly in terms of space since it will be using the stack for function calls
+//  }
+
+// int recursive_count_tail(struct Node* p, int acc){
+//     if (p == NULL)
+//         return acc;
+//     return recursive_count_tail(p->next, acc + 1);
+// }
+
+// | Type           | Time Complexity | Space Complexity |
+// | -------------- | --------------- | ---------------- |
+// | Iterative      | O(n)            | O(1)             |
+// | Recursive      | O(n)            | O(n)             |
+// | Tail Recursive | O(n)            | O(n) (in C)      |
+
+// adding data in nodes using iteration
+int add_nodes(struct Node *p)
+{
+    int sum;
+    sum = 0;
+    printf("\n");
+    while (p != NULL)
+    {
+        sum = sum + p->data;
+        p = p->next;
+    }
+    printf("The sum of data in the nodes is: %d \n", sum);
+
+    return sum;
+
+    // time complexity - O(n)
+    // space complexity - O(1)
+}
+
+int add_nodes_recursion(struct Node *p)
+{
+    int sum;
+    sum = 0;
+    printf("\n");
+    if (p == NULL)
+
+        return 0;
+    return add_nodes_recursion(p->next) + p->data;
+}
 
 int main()
 {
@@ -155,9 +220,7 @@ int main()
     display_linked_list(head);
     display_recursive(head);
     printf("the number of nodes in the linked list are: %d \n", count_nodes(head));
-
-    
-
+    printf("the sum of data in nodes is: %d \n", add_nodes(head));
 
     return 0;
 }
